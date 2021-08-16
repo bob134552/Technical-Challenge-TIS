4bb00071e125c4f9217399fcea8a4e7d3a236e5d
bob134552 <bob134552@hotmail.com>
2021-08-16 01:40:50 +0100

    3. Added function to notify user next available date or dates if available from the same number of days before or after selected date.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 // Declare scottish bank holidays from https://www.gov.uk/bank-holidays#scotland
 const bankHolidays = [
-"01/01/2021",
-"04/01/2021",
-"02/04/2021",
-"03/05/2021",
-"31/05/2021",
-"02/08/2021",
-"30/11/2021",
-"27/12/2021",
-"28/12/2021",
+    "01/01/2021",
+    "04/01/2021",
+    "02/04/2021",
+    "03/05/2021",
+    "31/05/2021",
+    "02/08/2021",
+    "30/11/2021",
+    "27/12/2021",
+    "28/12/2021",
 ];
 
 $(document).ready(function () {
@@ -38,7 +38,7 @@ $(document).ready(function () {
 // Stores csv data and parses into array of objects.
 var data = [];
 csvData = $.ajax({
-    url: "",
+    url: "https://raw.githubusercontent.com/bob134552/Technical-Challenge-TIS/main/assets/events_2020.csv",
     method: "GET",
     headers: {
     }}).then(response => {
@@ -46,12 +46,34 @@ csvData = $.ajax({
 });
 
 $("#datepicker").change(function () {
-    var selectedDate = this.value;
     var nextAvailable, prevAvailable, nextDate, prevDate;
+    var selectedDate = this.value;
     var messageContainer = $("#message");
 
     if (data[0].find((x) => x.Day === selectedDate) === undefined) {
         // If date is available.
         messageContainer.html(`${this.value} is available to book.`);
-    }
+    } else if (data[0].find((x) => x.Day === selectedDate) !== undefined) {
+        index = data[0].indexOf(data[0].find((x) => x.Day === selectedDate));
+        band = data[0][index].Booking;
+
+        for (var i = 1; ; i++) {
+            nextAvailable = moment(selectedDate, "DDMMYYYY").add(i, "days");
+            prevAvailable = moment(selectedDate, "DDMMYYYY").subtract(i, "days");
+
+            // If either way is equal days from selected date.
+            if (data[0].find((x) => x.Day === prevAvailable.format("L")) === undefined && bankHolidays.find(day => day === prevAvailable.format("L")) === undefined && prevAvailable.format("dddd") !== "Wednesday" &&
+                data[0].find((x) => x.Day === nextAvailable.format("L")) === undefined && bankHolidays.find(day => day === nextAvailable.format("L")) === undefined && nextAvailable.format("dddd") !== "Wednesday") {
+                nextDate = nextAvailable.format("L");
+                prevDate = prevAvailable.format("L");
+                messageContainer.html(`${this.value} is unavailable to book as ${band} is on this day. The next available date is ${prevDate} or ${nextDate}.`);
+                break;
+            // If next day is sooner than equal days from selected date.
+            } else if (data[0].find((x) => x.Day === nextAvailable.format("L")) === undefined && bankHolidays.find(day => day === nextAvailable.format("L")) === undefined && nextAvailable.format("dddd") !== "Wednesday") {
+                nextDate = nextAvailable.format("L");
+                messageContainer.html(`${this.value} is unavailable to book as ${band} is on this day. The next available date is ${nextDate}.`);
+                break;
+            };
+        };
+    };
 });
